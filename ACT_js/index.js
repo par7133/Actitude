6b3d91296a373d26d87f87580a4a756ad1948f6d
Daniele Bonini (皮夕): WebDev and DevOps by lots of Simplicity bits <my25mb@aol.com>
2021-11-20 11:33:44 +0100

    Add files via upload

--- ACT_js/index.js
+++ ACT_js/index.js
@@ -1,20 +1,20 @@
 /**
  * Copyright 2021, 2024 5 Mode
  *
- * This file is part of Homomm.
+ * This file is part of Actitude.
  *
- * Homomm is free software: you can redistribute it and/or modify
+ * Actitude is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Homomm is distributed in the hope that it will be useful,
+ * Actitude is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.  
  * 
  * You should have received a copy of the GNU General Public License
- * along with Homomm. If not, see <https://www.gnu.org/licenses/>.
+ * along with Actitude. If not, see <https://www.gnu.org/licenses/>.
  *
  * home.js
  * 
