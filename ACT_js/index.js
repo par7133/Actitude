28aa21526ee2a4a2a976f3976b7f58dcf851dffa
Daniele Bonini (皮夕): WebDev and DevOps by lots of Sim.pli.city bits <my25mb@aol.com>
2022-07-26 03:06:08 +0200

    Update index.js

--- ACT_js/index.js
+++ ACT_js/index.js
@@ -1,20 +1,20 @@
 /**
  * Copyright 2021, 2024 5 Mode
  *
- * This file is part of Actitude.
+ * This file is part of Homohug.
  *
- * Actitude is free software: you can redistribute it and/or modify
+ * Homohug is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Actitude is distributed in the hope that it will be useful,
+ * Homohug is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.  
  * 
  * You should have received a copy of the GNU General Public License
- * along with Actitude. If not, see <https://www.gnu.org/licenses/>.
+ * along with Homohug. If not, see <https://www.gnu.org/licenses/>.
  *
  * home.js
  * 
@@ -155,7 +155,7 @@ function setContentPos() {
     //$("#contentbar").css("width","100%");
     $("#logo-hmm").css("display","none");
   } else {  
-    $("#ahome").attr("href","http://actitu.de");
+    $("#ahome").attr("href","http://homohug.org");
     $("#agithub").css("display","inline");
     $("#afeedback").css("display","inline");
     $("#asupport").css("display","inline");  
